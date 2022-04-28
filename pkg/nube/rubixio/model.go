d55b9b2bd1372ae701a6003213ea6dc0943b73a5
Aidan P <aidan@enviroservices.com.au>
2022-04-28 12:07:46 +1000

    update to rcio

--- pkg/nube/rubixio/model.go
+++ pkg/nube/rubixio/model.go
@@ -6,72 +6,73 @@ type Write struct {
 }
 
 type Ping struct {
-	Ok bool `json:"ok"`
+	Ok     bool `json:"ok"`
+	PigPIO bool `json:"pigio_is_running"`
 }
 
 type Inputs struct {
 	UI1 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI1"`
 	UI2 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI2"`
 	UI3 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI3"`
 	UI4 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI4"`
 	UI5 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI5"`
 	UI6 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI6"`
 	UI7 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI7"`
 	UI8 struct {
-		IoNum   string  `json:"io_num"`
-		Raw     int     `json:"raw"`
-		Temp10K float64 `json:"temp_10_k"`
-		Volt    float64 `json:"volt"`
-		Current float64 `json:"current"`
-		Bool    int     `json:"bool"`
+		IoNum              string  `json:"io_num"`
+		Raw                int     `json:"raw"`
+		Thermistor10KType2 float64 `json:"thermistor_10k_type_2"`
+		VoltageDc          float64 `json:"voltage_dc"`
+		Current            float64 `json:"current"`
+		Digital            int     `json:"digital"`
 	} `json:"UI8"`
 }
